ed1a0b4b3db49eca2a47f40ee4bc007f53765f1c
pavelbeard <weewlol1337@gmail.com>
2021-12-10 17:26:35 +0300

    updated TestClass.cs

--- git-test-app/git-test-app/test-folder/TestClass.cs
+++ git-test-app/git-test-app/test-folder/TestClass.cs
@@ -12,5 +12,8 @@ namespace git_test_app.test_folder
 
         public void HelloWorld() => Console.WriteLine("HelloWorld!");
         public void HelloMoscow() => Console.WriteLine("HelloMoscow!");
+
+        public void Thanksgiving(string give) { Console.WriteLine(give); }
+        public void Thanksgiving(int give) { Console.WriteLine(give); }
     }
 }
