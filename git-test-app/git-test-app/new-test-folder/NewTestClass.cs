a58f1481d333ed20131e248ce50b49c90a48fecc
pavelbeard <weewlol1337@gmail.com>
2021-12-10 17:25:16 +0300

    added new-test-folder with class NewTestClass.cs

---
+++ git-test-app/git-test-app/new-test-folder/NewTestClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_test_app.new_test_folder
+{
+    internal class NewTestClass
+    {
+        public string test = "2";
+
+        public void HelloWorld() => Console.WriteLine("HelloWorld!");
+        public void HelloMoscow() => Console.WriteLine("HelloMoscow!");
+
+        public void Thanksgiving(string give) { Console.WriteLine(give); }
+        public void Thanksgiving(int give) { Console.WriteLine(give); }
+    }
+}
